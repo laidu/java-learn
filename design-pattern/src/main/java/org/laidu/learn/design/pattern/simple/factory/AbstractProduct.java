14525d3769e08483368bed40201a5387b4c820f4
Tiancai ZANG <tiancai.zang@cafintech.com>
2017-09-01 20:42:22 +0800

    开始 工厂模式学习

---
+++ design-pattern/src/main/java/org/laidu/learn/design/pattern/simple/factory/AbstractProduct.java
@@ -0,0 +1,27 @@
+package org.laidu.learn.design.pattern.simple.factory;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * 抽象工厂
+ * <p>
+ * Created by 臧天才 on 2017-09-01 18:10.
+ */
+@Slf4j
+// TODO : 2017-09-01 18:10  抽象产品
+public abstract class AbstractProduct {
+
+    protected ColorType color;
+
+    AbstractProduct(ColorType color){
+        this.color = color;
+    }
+
+    public enum ColorType{
+
+        RED,
+        BLACK,
+        BLUE,
+        YELLOW;
+    }
+}
---
+++ design-pattern/src/main/java/org/laidu/learn/design/pattern/simple/factory/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * 简单工厂
+ * <p>
+ * Created by Intellij idea on 2017-09-01 17:57.
+ */
+// TODO : 2017-09-01 17:57  简单工厂
+package org.laidu.learn.design.pattern.simple.factory;
