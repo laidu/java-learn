e2271b31044cba6bfa2c24d8122b8085894ddc51
Tiancai ZANG <tiancai.zang@cafintech.com>
2017-09-06 11:57:55 +0800

    完成 抽象工厂模式

--- design-pattern/src/main/java/org/laidu/learn/design/pattern/_abstract/factory/package-info.java
+++ design-pattern/src/main/java/org/laidu/learn/design/pattern/_abstract/factory/package-info.java
@@ -3,5 +3,5 @@
  * <p>
  * Created by Intellij idea on 2017-09-01 10:47.
  */
-// TODO : 2017-09-01 10:47  抽象工厂
+//  : 2017-09-01 10:47  抽象工厂
 package org.laidu.learn.design.pattern._abstract.factory;
--- design-pattern/src/main/java/org/laidu/learn/design/pattern/factory/package-info.java
+++ design-pattern/src/main/java/org/laidu/learn/design/pattern/factory/package-info.java
@@ -3,5 +3,5 @@
  * <p>
  * Created by Intellij idea on 2017-09-01 10:49.
  */
-// TODO : 2017-09-01 10:49  工厂模式
+//  : 2017-09-01 10:49  工厂模式
 package org.laidu.learn.design.pattern.factory;
