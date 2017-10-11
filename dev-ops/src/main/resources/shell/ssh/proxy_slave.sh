184e226a2d163b3eef02a3e5eacb7a61b323a7d2
zangtiancai <laidu823@gmail.com>
2017-10-11 15:17:11 +0800

    add ssh

--- dev-ops/src/main/resources/shell/ssh/proxy_slave.sh
+++ dev-ops/src/main/resources/shell/ssh/proxy_slave.sh
@@ -10,10 +10,10 @@
 ';
 
 ARGS=$1
-USERNAME=$2
+LOGIN_NAME=$2
 PASSWORD=$3
 
 ip_address= echo ${ARGS} | cut -d ':' -f1
 ip_port= echo ${ARGS} | cut -d ':' -f2
 
-sshpass -p ${PASSWORD} ssh ${USERNAME}@${ip_address} -p ${ip_port}
+sshpass -p "${PASSWORD}" ssh ${LOGIN_NAME}@${ip_address} -p ${ip_port}
