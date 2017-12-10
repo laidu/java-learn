f744273d9e1c20d6ad66b543c3cd463794cea3f2
laidu <laidu823@gmail.com>
2017-12-10 22:02:25 +0800

    add swagger module

---
+++ framework/swagger/src/main/java/org/laidu/learn/swagger/demo/controller/HomeController.java
@@ -0,0 +1,37 @@
+package org.laidu.learn.swagger.demo.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.laidu.learn.swagger.demo.model.Product;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * home
+ * <p>
+ * Created by tiancai.zang on 2017-12-10 21:43.
+ */
+@Slf4j
+@RestController
+@RequestMapping("/")
+public class HomeController {
+
+    @GetMapping({"/","/index","/home"})
+    public String home(){
+        return "hello world";
+    }
+
+    @GetMapping("/query/{id}")
+    public Product query(@PathVariable String id){
+        return Product.builder()
+                .id(id)
+                .name("test1")
+                .productDate(System.currentTimeMillis()+"")
+                .build();
+    }
+
+    @PostMapping("/add")
+    public Product add(Product product){
+        return product;
+    }
+
+
+}
