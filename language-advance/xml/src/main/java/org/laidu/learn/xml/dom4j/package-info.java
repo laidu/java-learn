9b09079496cf6a9166bef5284d17b8c95e1353c2
臧天才 <tiancai.zang@cafintech.com>
2017-12-11 13:55:51 +0800

    add xml module

---
+++ language-advance/xml/src/main/java/org/laidu/learn/xml/dom4j/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * dom4j xml
+ * <p>
+ * Created by tiancai.zang on 2017-12-11 13-55.
+ */
+package org.laidu.learn.xml.dom4j;
---
+++ language-advance/xml/src/main/java/org/laidu/learn/xml/jsoup/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * jsoup xml
+ * <p>
+ * Created by tiancai.zang on 2017-12-11 13-55.
+ */
+package org.laidu.learn.xml.jsoup;
