ec766419e5bec6151329b1cf2fc5550df852bd9a
臧天才 <laidu823@sina.com>
2017-12-20 14:11:51 +0800

    add leetcode.twoSum

---
+++ algorithm/src/main/java/org/laidu/learn/algorithm/leetcode/TwoSum.java
@@ -0,0 +1,27 @@
+package org.laidu.learn.algorithm.leetcode;
+
+import java.util.HashMap;
+
+/**
+ * Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+ * You may assume that each input would have exactly one solution, and you may not use the same element twice.
+ * Example:
+ * Given nums = [2, 7, 11, 15], target = 9, Because nums[0] + nums[1] = 2 + 7 = 9, return [0, 1].
+ * <p>
+ * Created by tiancai.zang on 2017-12-20 14-08.
+ */
+public class TwoSum {
+
+    public int[] twoSum(int[] nums, int target) {
+        int len = nums.length;
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < len; ++i) {
+            if (map.containsKey(nums[i])) {
+                return new int[]{map.get(nums[i]), i};
+            }
+            map.put(target - nums[i], i);
+        }
+        return null;
+    }
+
+}
