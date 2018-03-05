ed7a7657dcfafc22c631a25acce32d32bb951f2d
tiancai.zang <tiancai.zang@cafintech.com>
2018-03-05 15:13:17 +0800

    add service-register-center service-hello consumer-hello moudle

---
+++ framework/spring/spring-cloud/eureka-server/src/main/java/org/laidu/learn/spring/cloud/eureka/server/EurekaServer.java
@@ -0,0 +1,16 @@
+package org.laidu.learn.spring.cloud.eureka.server;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * eurrka server
+ *
+ * @author tiancai.zang
+ * @date 2018-03-05 11:35.
+ */
+@Slf4j
+public class EurekaServer {
+
+
+}
---
+++ framework/spring/spring-cloud/service-hello/src/main/java/org/laidu/learn/spring/cloud/ServiceHelloApplication.java
@@ -0,0 +1,14 @@
+package org.laidu.learn.spring.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class ServiceHelloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceHelloApplication.class, args);
+	}
+}
