85ffe064535374cfdd4d51128a7ff7aa9a382756
Tiancai ZANG <laidu823@sina.com>
2018-03-23 15:16:38 +0800

    add hexo docker

---
+++ dev-ops/src/main/resources/docker/image/hexo/init_env.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+#   function :
+#        init hexo env
+#    require :
+#        node git
+#    usage :
+#            1. sh init_env.sh
+
+npm install -g hexo
+npm install hexo-migrator-wordpress --save
+npm install hexo-generator-feed --save
+npm install hexo-renderer-marked --save
+npm install hexo-generator-search --save
+npm install hexo-autonofollow --save
+npm install hexo-generator-sitemap --save
+npm install hexo-generator-json-content --save
+npm install hexo-wordcount --save
+npm install hexo-generator-restful --save
+npm install hexo-renderer-marked --save
+npm install hexo-renderer-stylus --save
