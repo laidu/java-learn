7a0d6a3e066404f9c12d11c73b4d1763efe5b027
Tiancai ZANG <laidu823@sina.com>
2018-03-26 16:50:44 +0800

    update elk to -platinum

--- dev-ops/src/main/resources/docker/shell/es.sh
+++ dev-ops/src/main/resources/docker/shell/es.sh
@@ -1,12 +1,16 @@
 #! /bin/bash
 mysql_root=/data/docker/mysql
 #docker run  -d --restart=always \
-docker run --rm --name es-dev  \
+docker run --rm -d --name es-dev  \
 	-p 0.0.0.0:9200:9200 \
 	-p 0.0.0.0:9300:9300 \
 	-e discovery.type=single-node \
+	-e xpack.license.self_generated.type=trial \
 	-e xpack.security.enabled=false \
 	-e ELASTIC_PASSWORD=es-dev \
-	-v /data/docker/elk/elasticsearch/data:/usr/share/elasticsearch/data  \
+	-v /Users/laidu/Files/data/docker/elasticsearch/config/elasticsearch.yml:/usr/share/elasticsearch/config/elasticsearch.yml \
+	-v /Users/laidu/Files/data/docker/elasticsearch/data:/usr/share/elasticsearch/data \
+	-v /Users/laidu/Files/data/docker/upload/x-pack-core-6.2.2.jar:/usr/share/elasticsearch/plugins/x-pack/x-pack-core/x-pack-core-6.2.2.jar \
+	-v /Users/laidu/Files/data/docker/upload:/root \
 	-v /etc/hosts:/etc/hosts  \
 	daocloud.io/laidu/elasticsearch_6
--- dev-ops/src/main/resources/docker/shell/kibana.sh
+++ dev-ops/src/main/resources/docker/shell/kibana.sh
@@ -1,8 +1,9 @@
 #! /bin/bash
 #docker run  -d --restart=always \
-docker run  --rm --name kibana-dev 
-	-e elasticsearch.password=es-dev 
+docker run  --rm -d --name kibana-dev \
+	-e elasticsearch.password=es-dev \
 	-p 0.0.0.0:5601:5601 \
 	-v /etc/hosts:/etc/hosts \
-	-v /data/docker/elk/kinaba/config/kibana.yml:/usr/share/kibana/config/kibana.yml  \
+	-v /Users/laidu/Files/data/docker/upload:/root \
+	-v /Users/laidu/Files/data/docker/kinaba/config/kibana.yml:/usr/share/kibana/config/kibana.yml  \
 	daocloud.io/laidu/kibana_6
