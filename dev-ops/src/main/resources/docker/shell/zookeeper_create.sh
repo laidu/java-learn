ced0a2bc5f0e0602b28697b4879bacd241e75d95
Tiancai ZANG <laidu823@sina.com>
2018-04-03 18:57:00 +0800

    sync

--- dev-ops/src/main/resources/docker/shell/zookeeper_create.sh
+++ dev-ops/src/main/resources/docker/shell/zookeeper_create.sh
@@ -1,6 +1,10 @@
 #! /bin/bash
-name=$1
-port1=$2
-port2=$3
-port3=$4
-docker run --name zookeeper-$name -p0.0.0.0:$port1:2181 -p0.0.0.0:$port2:2888 -p0.0.0.0:$port3:3888  --restart always -d zookeeper
+
+kibana_root=/Users/laidu/Files/data/docker/kibana
+pubilc_path=/Users/laidu/Files/data/docker/upload
+docker run --name zookeeper-$name -p0.0.0.0:$port1:2181 -p0.0.0.0:$port2:2888 -p0.0.0.0:$port3:3888   -d zookeeper
+
+#docker run --restart always \
+docker run --rm -d --net=host \
+    -v ${pubilc_path}/upload:/root \
+    --name zookeeper-dev zookeeper
