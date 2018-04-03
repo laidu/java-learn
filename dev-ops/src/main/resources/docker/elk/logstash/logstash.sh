83fd7486c1685343a36dd6d538e7cae176f001a5
Tiancai ZANG <laidu823@sina.com>
2018-04-03 16:28:11 +0800

    add oracle java 8

--- dev-ops/src/main/resources/docker/elk/logstash/logstash.sh
+++ dev-ops/src/main/resources/docker/elk/logstash/logstash.sh
@@ -3,7 +3,10 @@
 logstash_root=/Users/laidu/Files/data/docker/logstash
 pubilc_path=/Users/laidu/Files/data/docker/upload
 
-docker run --rm -it \
-    -v ${logstash_root}/config/logstash.yml:/usr/share/logstash/config/logstash.yml \
-    -v ${pubilc_path}/upload:/root \
-    daocloud.io/laidu/kibana_6
+#docker run  -d --restart=always \
+docker run  --rm -d --net=host --name logstash-dev \
+	-v /etc/hosts:/etc/hosts \
+	-v ${logstash_root}/config:/usr/share/logstash/config \
+	-v ${logstash_root}/data:/usr/share/logstash/data \
+	-v ${pubilc_path}/upload:/root \
+	daocloud.io/laidu/elk-logstash6
