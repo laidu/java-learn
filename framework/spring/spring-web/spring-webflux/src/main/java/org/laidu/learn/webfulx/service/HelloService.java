102d6182df7adb8c5e785fe158d7d5d4f746b163
ztc <laidu823@sina.com>
2018-06-15 16:11:02 +0800

    skip test error

---
+++ framework/spring/spring-web/spring-webflux/src/main/java/org/laidu/learn/webfulx/service/HelloService.java
@@ -0,0 +1,11 @@
+package org.laidu.learn.webfulx.service;
+
+/**
+ * hello service
+ * <p>
+ * Created by tiancai.zang
+ * on 2018-06-15 15:25.
+ */
+// TODO: 2018-06-15 15:25  hello service
+public interface HelloService {
+}
---
+++ framework/spring/spring-web/spring-webflux/src/main/java/org/laidu/learn/webfulx/service/impl/HelloServiceImpl.java
@@ -0,0 +1,17 @@
+package org.laidu.learn.webfulx.service.impl;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by tiancai.zang
+ * on 2018-06-15 15:27.
+ *
+ * @author tiancai.zang
+ */
+// TODO: 2018-06-15 15:27  
+@Slf4j
+@Service
+public class HelloServiceImpl {
+
+}
