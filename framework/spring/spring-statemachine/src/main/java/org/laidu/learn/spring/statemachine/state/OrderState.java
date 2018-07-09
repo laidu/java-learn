9390137b2a236aead76375914ba845fdb6f1c070
ztc <laidu823@sina.com>
2018-07-09 19:51:54 +0800

    状态机开始

---
+++ framework/spring/spring-statemachine/src/main/java/org/laidu/learn/spring/statemachine/state/OrderState.java
@@ -0,0 +1,17 @@
+package org.laidu.learn.spring.statemachine.state;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * 订单状态机
+ * <p>
+ * Created by laidu
+ * on 2018-07-09 19:50.
+ *
+ * @author laidu
+ */
+@Slf4j
+public class OrderState {
+
+    
+}
