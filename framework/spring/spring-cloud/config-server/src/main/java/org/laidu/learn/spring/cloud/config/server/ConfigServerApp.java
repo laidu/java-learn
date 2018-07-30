f7980ccaea941bcbaf2dbf4ed5bcfe40f7899c84
xpcoder823@gmail.com <xpcoder823@gmail.com>
2018-07-30 23:56:38 +0800

    完成 spring cloud config demo

---
+++ framework/spring/spring-cloud/config-server/src/main/java/org/laidu/learn/spring/cloud/config/server/ConfigServerApp.java
@@ -0,0 +1,28 @@
+package org.laidu.learn.spring.cloud.config.server;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.Banner;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * 配置服务
+ * <p>
+ * Created by tiancai.zang
+ * on 2018-07-30 21:30.
+ */
+@Slf4j
+@EnableConfigServer
+@SpringBootApplication
+public class ConfigServerApp {
+
+
+    public static void main(String[] args) {
+
+        new SpringApplicationBuilder(ConfigServerApp.class)
+                .logStartupInfo(false)
+                .bannerMode(Banner.Mode.OFF)
+                .run(args);
+    }
+}
