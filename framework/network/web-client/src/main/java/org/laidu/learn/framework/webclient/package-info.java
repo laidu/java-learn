2debc914a268b7b4ef4e0798a166ef6166bd3d1f
Tiancai ZANG <tiancai.zang@finlink.net.cn>
2018-08-06 14:47:19 +0800

    添加web-client

---
+++ framework/network/web-client/src/main/java/org/laidu/learn/framework/webclient/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * spring webclient 使用
+ * <p>
+ * Created by laidu
+ * on 2018-08-06 14:10.
+ *
+ * @author laidu
+ */
+// TODO: 2018-08-06 14:10  spring webclient 使用
+package org.laidu.learn.framework.webclient;
---
+++ framework/network/web-client/src/main/java/org/laidu/learn/framework/webclient/usage/Usage.java
@@ -0,0 +1,44 @@
+package org.laidu.learn.framework.webclient.usage;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import java.util.stream.IntStream;
+
+/**
+ * 使用
+ * <p>
+ * Created by laidu
+ * on 2018-08-06 14:10.
+ *
+ * @author laidu
+ */
+@Slf4j
+public class Usage {
+
+
+
+    public static void main(String[] args) {
+
+        IntStream.range(0,1000).parallel()
+                .forEach(index->{
+                    log.info("index : {}", index);
+                    WebClient.create()
+                            .get()
+                            .uri(uriBuilder -> uriBuilder
+                                    .scheme("h ttp")
+                                    .host("www.baidu.com")
+                                    .path("/s")
+                                    .queryParam("wd", "北京天气")
+                                    .queryParam("other", "test")
+                                    .build())
+                            .retrieve()
+                            .bodyToMono(String.class)
+                            .subscribe(resp -> log.info(": {}", resp));
+                });
+
+
+
+    }
+
+}
