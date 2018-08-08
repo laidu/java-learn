8a2078eae7cac4afeda662e33bf6fbfb2694112b
Tiancai ZANG <tiancai.zang@finlink.net.cn>
2018-08-08 18:42:45 +0800

    继续poi学习

--- framework/common/apache-poi/src/main/java/org/laidu/learn/apache/poi/create/CreateExcel.java
+++ framework/common/apache-poi/src/main/java/org/laidu/learn/apache/poi/create/CreateExcel.java
@@ -1,13 +1,16 @@
 package org.laidu.learn.apache.poi.create;
 
+import jodd.io.FileUtil;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.poi.xssf.usermodel.XSSFRow;
-import org.apache.poi.xssf.usermodel.XSSFSheet;
-import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.apache.poi.hssf.usermodel.HSSFCell;
+import org.apache.poi.hssf.usermodel.HSSFRow;
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.CellType;
 
 import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.stream.IntStream;
 
 /**
  * create excel
@@ -18,25 +21,43 @@ import java.io.IOException;
 @Slf4j
 public class CreateExcel {
 
+    private static final String workDir = System.getProperty("user.home")+"/temp/excel/";
+
+    static {
+        if (!FileUtil.isExistingFolder(new File(workDir))) {
+            try {
+                FileUtil.mkdir(new File(workDir));
+                System.out.println("成功创建 工作目录 : " + workDir);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+
+    public static void createHSSFWorkbook() throws IOException {
+
+        HSSFWorkbook workbook = new HSSFWorkbook();
+
+        HSSFSheet sheet0 = workbook.createSheet("sheet0");
+
+        HSSFRow row0 = sheet0.createRow(0);
+
+        IntStream.range(0,10).parallel().forEach(i -> {
+            HSSFCell cell = row0.createCell(i, CellType.STRING);
+            cell.setCellValue(i+"da");
+        });
+
+        workbook.write(new File(workDir+"/HSSFWorkbook.xls"));
+
+    }
+
 
 
     public static void main(String[] args) throws IOException {
 
-        XSSFWorkbook workbook = new XSSFWorkbook();
 
-        String excelPath = "/Users/laidu/IdeaProjects/java-learn/framework/common/apache-poi/src/main/resources/excel/";
-        FileOutputStream out = new FileOutputStream(new File(excelPath +"createworkbook.xlsx"));
-
-        XSSFSheet defalut = workbook.createSheet("default1");
-        XSSFRow row = defalut.createRow(1);
-//        IntStream.range(0,10).forEach(index -> row.createCell(index,CellType.NUMERIC));
-
-
-
-
-        workbook.write(out);
-        out.close();
-        System.out.println("demo.xlsx written successfully");
+        createHSSFWorkbook();
 
     }
 }
---
+++ framework/common/apache-poi/src/site/markdown/index.md
@@ -0,0 +1,14 @@
+# apache poi 学习
+
+## apache poi 介绍
+
+> Apache POI项目是用于开发基于Microsoft的OLE 2复合文档格式的纯Java文件格式端口的主项目。 OLE 2复合文档格式由Microsoft Office Documents以及使用MFC属性集来序列化其文档对象的程序使用。
+
+
+参考：
+* [官方示例](https://poi.apache.org/components/index.html)
+* [java doc](https://poi.apache.org/apidocs/index.html)
+* []()
+* [POI教程](https://wizardforcel.gitbooks.io/tutorialspoint-java/apache-poi/1.html)
+* []()
+* []()
