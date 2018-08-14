d3976ef0eb4fa9545b8c32a2b4c657b1a558646c
xpcoder823@gmail.com <xpcoder823@gmail.com>
2018-08-14 20:55:12 +0800

    sync

--- framework/spring/spring-boot/spring-boot-sso/src/main/java/org/laidu/learn/spring/boot/sso/SSOApp.java
+++ framework/spring/spring-boot/spring-boot-sso/src/main/java/org/laidu/learn/spring/boot/sso/SSOApp.java
@@ -12,11 +12,10 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
  * @author tiancai.zang
  * on 2018-08-10 18:54.
  */
-// TODO: 2018-08-10 18:54  sso 启动类
 @Slf4j
 @SpringBootApplication
 @EnableOAuth2Sso
-public class SSOApp {
+public class SSOApp{
 
     public static void main(String[] args) {
         new SpringApplicationBuilder(SSOApp.class)
