6603f9e90ceec436111c75042c81ca12afb78bd8
Tiancai ZANG <tiancai.zang@finlink.net.cn>
2018-08-24 18:08:42 +0800

    sync

---
+++ framework/spring/spring-boot/spring-boot-admin/src/main/java/org/laidu/learn/spring/boot/admin/conf/NotifierConfiguration.java
@@ -0,0 +1,45 @@
+package org.laidu.learn.spring.boot.admin.conf;
+
+import de.codecentric.boot.admin.server.domain.entities.Instance;
+import de.codecentric.boot.admin.server.domain.entities.InstanceRepository;
+import de.codecentric.boot.admin.server.domain.events.InstanceEvent;
+import de.codecentric.boot.admin.server.domain.events.InstanceStatusChangedEvent;
+import de.codecentric.boot.admin.server.notify.AbstractEventNotifier;
+import de.codecentric.boot.admin.server.notify.Notifier;
+import de.codecentric.boot.admin.server.notify.RemindingNotifier;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import reactor.core.publisher.Mono;
+
+import java.time.Duration;
+
+/**
+ * notify conf
+ *
+ * @author tiancai.zang
+ * on 2018-08-24 17:53.
+ */
+@Slf4j
+@Configuration
+public class NotifierConfiguration  extends AbstractEventNotifier {
+
+    public NotifierConfiguration(InstanceRepository repository) {
+        super(repository);
+    }
+
+    @Override
+    protected Mono<Void> doNotify(InstanceEvent event, Instance instance) {
+        return Mono.fromRunnable(() -> {
+            if (event instanceof InstanceStatusChangedEvent) {
+                log.info("Instance {} ({}) is {}", instance.getRegistration().getName(), event.getInstance(),
+                        ((InstanceStatusChangedEvent) event).getStatusInfo().getStatus());
+            } else {
+                log.info("Instance {} ({}) {}", instance.getRegistration().getName(), event.getInstance(),
+                        event.getType());
+            }
+        });
+    }
+}
