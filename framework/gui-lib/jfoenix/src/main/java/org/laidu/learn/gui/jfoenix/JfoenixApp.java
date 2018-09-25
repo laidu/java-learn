59b4bebc01bca1c4f513bc9f5cec366f5c93b5a8
xpcoder823@gmail.com <xpcoder823@gmail.com>
2018-09-25 23:23:37 +0800

    sync

---
+++ framework/gui-lib/jfoenix/src/main/java/org/laidu/learn/gui/jfoenix/JfoenixApp.java
@@ -0,0 +1,20 @@
+package org.laidu.learn.gui.jfoenix;
+
+import javafx.embed.swing.JFXPanel;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * jfoenix gui app
+ * <p>
+ * Created by tiancai.zang
+ * on 2018-09-25 23:02.
+ */
+@Slf4j
+public class JfoenixApp {
+
+    public static void main(String[] args) {
+
+        JFXPanel jfxPanel = new JFXPanel();
+
+    }
+}
