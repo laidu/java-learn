b36b624dad8868eee2fcd4c240f01968050dd6db
Tiancai ZANG <tiancai.zang@finlink.net.cn>
2018-10-12 19:00:19 +0800

    remove spring boot admin 2 spring cloud

--- framework/spring/spring-cloud/admin-dashboard/src/main/java/org/laidu/learn/spring/boot/admin/AdminApp.java
+++ framework/spring/spring-cloud/admin-dashboard/src/main/java/org/laidu/learn/spring/boot/admin/AdminApp.java
@@ -5,6 +5,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.Banner;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Configuration;
 
 /**
@@ -15,6 +16,7 @@ import org.springframework.context.annotation.Configuration;
  */
 @Slf4j
 @Configuration
+@EnableDiscoveryClient
 @EnableAutoConfiguration
 @EnableAdminServer
 public class AdminApp {
--- framework/spring/spring-cloud/api-gateway/src/main/java/org/laidu/learn/spring/cloud/service/api/ApiGatewayApp.java
+++ framework/spring/spring-cloud/api-gateway/src/main/java/org/laidu/learn/spring/cloud/service/api/ApiGatewayApp.java
@@ -2,8 +2,10 @@ package org.laidu.learn.spring.cloud.service.api;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.Banner;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 /**
@@ -14,14 +16,16 @@ import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
  */
 @Slf4j
 @EnableZuulProxy
+@EnableDiscoveryClient
 @SpringBootApplication
-public class ServiceApiApp {
+public class ApiGatewayApp {
 
     public static void main(String[] args) {
 
-        new SpringApplicationBuilder(ServiceApiApp.class)
+        new SpringApplicationBuilder(ApiGatewayApp.class)
                 .bannerMode(Banner.Mode.OFF)
                 .logStartupInfo(false)
+                .web(WebApplicationType.SERVLET)
                 .run(args);
     }
 }
