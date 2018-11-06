43b4e4a258262e37fd91a7f0ca0f57d3247eeffb
Tiancai ZANG <laidu823@gmail.com>
2018-11-06 20:38:06 +0800

    use settled reply queue

--- language-feature/src/main/java/org/laidu/learn/language/feature/string/StringDemo.java
+++ language-feature/src/main/java/org/laidu/learn/language/feature/string/StringDemo.java
@@ -13,12 +13,23 @@ public class StringDemo {
 
     public static void main(String[] args) {
 
-
+        // 会在栈中创建一个对象引用变量str，然后查看字符串池中是否存在”XXX”，如果没有，则将”XXX”存放字符串池，并令引用变量str指向它；如果已经有”XXX”，则直接令str指向它
         String s = "hello";
 
+        // 是java中标准的对象创建方式，其创建的对象将直接放置到堆中，每调用一次就会在堆上面创建一个新的对象
         String s1 = new String("hello");
 
+        // intern() 用于返回一个String池中的对象，如果池中没有该对象则先将该String放入String池中然后返回该对象
+        String s2 = new String("hello").intern();
+
         System.out.println("s == s1 : " + (s == s1));
+        System.out.println("s == s2 : " + (s == s2));
+
+
+        // System.identityHashCode(s) 获取对象内存地址，不管该对象是否重写hashCode()
+        System.out.println("s\t" + System.identityHashCode(s));
+        System.out.println("s1\t" + System.identityHashCode(s1));
+        System.out.println("s2\t" + System.identityHashCode(s2));
 
         System.out.println("s.equals(s1) : " + s.equals(s1));
 
