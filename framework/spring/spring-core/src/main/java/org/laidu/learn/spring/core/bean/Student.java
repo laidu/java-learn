290d243228b5c3ee07f73e164dc1a58839c6cdae
zangtiancai <zangtiancai@baidu.com>
2019-02-13 20:09:24 +0800

    add spring core module

---
+++ framework/spring/spring-core/src/main/java/org/laidu/learn/spring/core/bean/Student.java
@@ -0,0 +1,60 @@
+package org.laidu.learn.spring.core.bean;
+
+import lombok.Data;
+import lombok.ToString;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * student bean
+ * <p>
+ * Created by zangtiancai.
+ * on 2019-02-13 19:29.
+ */
+@Slf4j(topic = "student")
+@Data
+@ToString
+public class Student {
+
+    private String name;
+
+    private String idCard;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        log.info("===========setName===========: {}",name);
+        this.name = name;
+    }
+
+    public String getIdCard() {
+        return idCard;
+    }
+
+    public void setIdCard(String idCard) {
+        log.info("===========setIdCard===========: {}",idCard);
+        this.idCard = idCard;
+    }
+
+    public Student() {
+        log.info("===========无参构造器===========");
+    }
+
+    public Student(String name, String idCard) {
+        this.name = name;
+        this.idCard = idCard;
+       log.info("===========全参构造器===========");
+
+    }
+
+    public Student(String name) {
+        this.name = name;
+        log.info("===========name参构造器===========");
+    }
+
+    public void init(){
+        log.info("===========init方法===========");
+    }
+
+}
---
+++ framework/spring/spring-core/src/main/java/org/laidu/learn/spring/core/package-info.java
@@ -0,0 +1,8 @@
+package org.laidu.learn.spring.core;
+
+/**
+ * spring core 学习
+ * <p>
+ * Created by zangtiancai.
+ * on 2019-02-13 19:25.
+ */
