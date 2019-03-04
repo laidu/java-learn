466985312e5393adb82c439640007057c457fec2
zangtiancai <zangtiancai@baidu.com>
2019-03-04 19:21:07 +0800

    HttpUrl

--- framework/network/retrofit/src/main/java/org/laidu/learn/network/retrofit/usage/github/GithubServiceApp.java
+++ framework/network/retrofit/src/main/java/org/laidu/learn/network/retrofit/usage/github/GithubServiceApp.java
@@ -1,14 +1,17 @@
 package org.laidu.learn.network.retrofit.usage.github;
 
 import lombok.extern.slf4j.Slf4j;
+import okhttp3.EventListener;
+import okhttp3.HttpUrl;
+import okhttp3.OkHttpClient;
 import org.laidu.learn.network.retrofit.usage.github.api.GitHubService;
-import org.laidu.learn.network.retrofit.usage.github.dto.ApiSummaryRes;
 import org.laidu.learn.network.retrofit.usage.github.dto.UserBasicInfo;
 import retrofit2.Response;
 import retrofit2.Retrofit;
 import xyz.xpcoder.common.retrofit.converter.fastjson.FastjsonConverterFactory;
 
 import java.io.IOException;
+import java.util.concurrent.TimeUnit;
 
 /**
  * github api app
@@ -22,22 +25,35 @@ public class GithubServiceApp {
 
     public static void main(String[] args) throws IOException {
 
+        String baseUrl = "http://";
+
+        OkHttpClient client = new OkHttpClient.Builder()
+                .retryOnConnectionFailure(true)
+                .connectTimeout(500, TimeUnit.MILLISECONDS)
+                .eventListener(new EventListener() {
+
+                })
+                .build();
+
+        HttpUrl httpUrl = new HttpUrl.Builder()
+                .scheme("http")
+                .host("api.github.com")
+                .port(80)
+                .build();
+
         Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl("https://api.github.com/")
+                .client(client)
+                .baseUrl(httpUrl)
                 .addConverterFactory(FastjsonConverterFactory.create())
                 .build();
 
         GitHubService gitHubService = retrofit.create(GitHubService.class);
 
-        Response<ApiSummaryRes> response = gitHubService.apiSummary()
-                .execute();
-
-
         String username = "laidu";
         Response<UserBasicInfo> userBasicInfo = gitHubService.getUserBasicInfo(username)
                 .execute();
 
-        log.info(": {}", userBasicInfo.body());
+        log.info("username : {}", userBasicInfo.body().getLogin());
 
 
     }
