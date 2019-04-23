3bd3ac87cbb1de1b821b82ad6000d9716aa7401a
ztc <laidu823@gmail.com>
2019-04-23 23:10:45 +0800

    .

--- framework/spring/spring-data/spring-mybatis/src/main/resources/db/schema.sql
+++ framework/spring/spring-data/spring-mybatis/src/main/resources/db/schema.sql
@@ -1,6 +1,9 @@
-
-create table app_user (
-    id bigint auto_increment,
-    username varchar(80) null,
-    id_card varchar(80) null
-);
+CREATE TABLE druid_demo.app_user (
+	id BIGINT NOT NULL AUTO_INCREMENT,
+	username varchar(100) NULL,
+	id_card varchar(100) NULL,
+	CONSTRAINT app_user_PK PRIMARY KEY (id)
+)
+ENGINE=InnoDB
+DEFAULT CHARSET=utf8mb4
+COLLATE=utf8mb4_general_ci;
