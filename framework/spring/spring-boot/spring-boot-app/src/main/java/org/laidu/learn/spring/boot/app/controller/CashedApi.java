3f09322e69df760e6acc26138ab01cb799b4c5d0
laidu <laidu823@gmail.com>
2020-06-11 23:08:22 +0800

    sync

--- framework/spring/spring-boot/spring-boot-app/src/main/java/org/laidu/learn/spring/boot/app/controller/CashedApi.java
+++ framework/spring/spring-boot/spring-boot-app/src/main/java/org/laidu/learn/spring/boot/app/controller/CashedApi.java
@@ -2,6 +2,7 @@ package org.laidu.learn.spring.boot.app.controller;
 
 import org.laidu.learn.spring.boot.app.service.ThreadLocalService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -20,6 +21,7 @@ import lombok.extern.slf4j.Slf4j;
 @RequestMapping("/cashed")
 public class CashedApi {
 
+    @Lazy
     @Autowired
     private ThreadLocalService threadLocalService;
 
